e2903a374a68d910c4fabd468e77fbd29ce76c06
srikant choubey <srikantchoubey2001@gmail.com>
2025-12-25 09:44:25 +0000

    Status Failed Name gcr.io/k8s-skaffold/pack Id pack Arguments /bin/sh -c

--- src/app/room/[roomId]/page.tsx
+++ src/app/room/[roomId]/page.tsx
@@ -2,6 +2,6 @@ import RoomPageClient from './page-client';
 
 // The 'searchParams' property is optional in Next.js page props.
 // Adding it to the type definition resolves the build error.
-export default function RoomPage({ params }: { params: { roomId: string }, searchParams: { [key: string]: string | string[] | undefined } }) {
+export default function RoomPage({ params }: { params: { roomId: string } }) {
   return <RoomPageClient roomId={params.roomId} />;
 }
